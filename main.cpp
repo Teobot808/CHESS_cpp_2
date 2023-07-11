ff47e3b460de247b9a023f1f2ee3158e3842b872
Teobot808 <85901342+Teobot808@users.noreply.github.com>
2023-07-11 03:46:31 +0200

    int piece

--- main.cpp
+++ main.cpp
@@ -5,11 +5,29 @@
 #include "Vector2.hpp"
 #include <string>
 
+struct Piece {
+
+const int WhiteKing = 75;
+const int BlackKing = 107;
+const int WhiteQueen = 81;
+const int BlackQueen = 113;
+const int WhiteBishop = 66;
+const int BlackBishop = 98;
+const int WhiteKnight = 78;
+const int BlackKnight = 110;
+const int WhitePawn = 80;
+const int BlackPawn = 112;
+const int WhiteRook = 82;
+const int BlackRook = 114;
+
+
+};
+
+
 
 
 int main(){
     //here we go!
-    std::cout << "IT WORKS" << std::endl;
-     system("pause");
+    
     return 0;
 }
