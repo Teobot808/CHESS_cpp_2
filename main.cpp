e8a04fbf90089aec512355ac4bb925b07cf0c2c0
Teobot808 <85901342+Teobot808@users.noreply.github.com>
2023-07-13 00:59:01 +0200

    graphics

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include "Vector2.hpp"
 #include <string>
+using namespace sf;
+
+int sizeofchessboard = 665;
+void Draw(RenderWindow &window, Sprite &chessboard, Sprite &bielaveza);
+float sizeofchessboard1 = (sizeofchessboard/665); //size
 
 struct Piece {
 
@@ -52,7 +57,61 @@ int b = chessboard[7];
 
 
     std::cout<<b<<std::endl;
-system("pause");
+//system("pause");
+
+
+
+
+
+//cancer
+sf::RenderWindow window(sf::VideoMode(665, 665), "My window");
+sf::Texture chessboardpng;
+        if (!chessboardpng.loadFromFile("chessboard1.png")) {
+            return 0;
+        }
+    sf::Sprite chessboard;
+        chessboard.setTexture(chessboardpng);
+        //chessboard.setPosition(sf::Vector2f(0.f, 0.f));
+                chessboard.setPosition(0, 0);
+chessboard.setScale(sizeofchessboard1, sizeofchessboard1);
+sf::Texture vezatexture;
+        if (!vezatexture.loadFromFile("Chess-Pieces.png", sf::IntRect(0, 0, 132, 132)))
+{
+    return 1; // error... 
+}
+sf::Sprite bielaveza;
+        bielaveza.setTexture(vezatexture);
+        //chessboard.setPosition(sf::Vector2f(0.f, 0.f));
+               bielaveza.setOrigin(66, 66);
+                bielaveza.setPosition(100, 100);
+        bielaveza.setScale(0.62973484848f, 0.62973484848f);
+while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+                while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+                }
+
+        window.clear();              
+       
+             //Update(keyTime, window, chessboard);
+	        Draw(window, chessboard, bielaveza);
+}
 
     return 0;
+}
+
+void Draw(RenderWindow &window, Sprite &chessboard, Sprite &bielaveza)
+{
+	window.clear(Color::White);
+
+	//Draw stuff
+	//window.draw(retard);
+    window.draw(chessboard);
+    window.draw(bielaveza);
+	window.display();
 }
